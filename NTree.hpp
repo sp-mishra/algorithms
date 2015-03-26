8622c52c2d91efcc69c761a1efd621b33b748c2c
BrainlessLabs <brainlesslabs@gmail.com>
2015-03-26 16:24:04 +0530

    Adding MPL2 license.

--- NTree.hpp
+++ NTree.hpp
@@ -1,8 +1,12 @@
 #ifndef BLIB_NTREE_HPP
 #define BLIB_NTREE_HPP
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// Author: BrainlessLibraries
+
 #include <vector>
-#include <functional>
 #include <boost/iterator/iterator_facade.hpp>
 
 namespace blib {
