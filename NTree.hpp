65e7b11c6adfb810e26d5ffe4c25c3c9ba8bdd40
BrainlessLabs <brainlesslabs@gmail.com>
2015-03-30 19:50:27 +0530

    Removed reference to boost shared_ptr

--- NTree.hpp
+++ NTree.hpp
@@ -10,8 +10,6 @@
 #include <stack>
 #include <functional>
 #include <boost/iterator/iterator_facade.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
 
 namespace blib {
   namespace container {
