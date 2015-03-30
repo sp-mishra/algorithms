ef84be7fc23ea1da76cca2c6ec28931d3f79f1b3
BrainlessLabs <brainlesslabs@gmail.com>
2015-03-30 19:51:41 +0530

    Update treemain.cpp

--- treemain.cpp
+++ treemain.cpp
@@ -1,13 +1,18 @@
+//#include "renderer/Render.hpp"
+#include "geometry/BasicGeometry.hpp"
 #include "containers/tree/NTree.hpp"
 #include <iostream>
 
-int main( int/* argc*/, char ** /*argv[]*/ ) {
-  typedef blib::container::tree::Node<int> Node;
-  typedef blib::container::tree::Tree<Node> Tree;
+typedef blib::container::tree::Node<int> Node;
+typedef blib::container::tree::Tree<Node> Tree;
+
+void nodeOperationsTest( ) {
+  std::cout << "Node operations start" << std::endl;
   Node r;
   Tree t;
   t.root( r );
   auto& n = t.root( );
+
   if ( !n ) {
     std::cout << "No data in root node" << std::endl;
   }
@@ -48,7 +53,66 @@ int main( int/* argc*/, char ** /*argv[]*/ ) {
       std::cout << rit->data( ) << " ";
     }
   }
-  std::cout << std::endl;
+  std::cout << "\nNode operations end" << std::endl;
+}
+
+void createTree( Tree& t ) {
+  Node l11, l12, l21, l22, l31, l32;
+  l11.data( 11 );
+  l12.data( 12 );
+  l21.data( 21 );
+  l22.data( 22 );
+  l31.data( 31 );
+  l32.data( 32 );
+
+  Node l1, l2, l3;
+  l1.data( 1 );
+  l2.data( 2 );
+  l3.data( 3 );
+
+  l1.addChild( l11 );
+  l1.addChild( l12 );
+
+  l2.addChild( l21 );
+  l2.addChild( l22 );
+
+  l3.addChild( l31 );
+  l3.addChild( l32 );
+
+  Node r;
+  r.data( 0 );
+  r.addChild( l1 );
+  r.addChild( l2 );
+  r.addChild( l3 );
+
+  t.root( r );
+}
+
+void preOrderTest( ) {
+  std::cout << "preOrderTest start" << std::endl;
+  Tree t;
+  createTree( t );
+  for ( auto it = t.pre_order_begin( ); it != t.pre_order_end( ); ++it ) {
+    auto n = *it;
+    if ( n ) {
+      std::cout << n.data( ) << " ";
+    }
+  }
+  std::cout << "\npreOrderTest end" << std::endl;
+}
+
+int main( int/* argc*/, char ** /*argv[]*/ ) {
+  try {
+    nodeOperationsTest( );
+    preOrderTest( );
+  }
+  catch ( std::exception& e ) {
+    std::cout << "exception = " << e.what( ) << std::endl;
+  }
+  catch ( ... ) {
+    std::cout << "unknown exception" << std::endl;
+  }
+
 
   int i;
   std::cin >> i;
