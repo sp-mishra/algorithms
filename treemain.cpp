004c06354b0755e74cfec8dceef9a0ae1266bc18
BrainlessLabs <brainlesslabs@gmail.com>
2015-03-31 19:21:57 +0530

    Update treemain.cpp

--- treemain.cpp
+++ treemain.cpp
@@ -56,32 +56,18 @@ void nodeOperationsTest( ) {
 
 void createTree( Tree& t ) {
   Node l11, l12, l21, l22, l31, l32;
-  l11.data( 11 );
-  l12.data( 12 );
-  l21.data( 21 );
-  l22.data( 22 );
-  l31.data( 31 );
-  l32.data( 32 );
+  l11.data( 11 );  l12.data( 12 );  l21.data( 21 );  l22.data( 22 );  l31.data( 31 );  l32.data( 32 );
 
   Node l1, l2, l3;
-  l1.data( 1 );
-  l2.data( 2 );
-  l3.data( 3 );
+  l1.data( 1 );  l2.data( 2 );  l3.data( 3 );
 
-  l1.addChild( l11 );
-  l1.addChild( l12 );
-
-  l2.addChild( l21 );
-  l2.addChild( l22 );
-
-  l3.addChild( l31 );
-  l3.addChild( l32 );
+  l1.addChild( l11 );  l1.addChild( l12 );
+  l2.addChild( l21 );  l2.addChild( l22 );
+  l3.addChild( l31 );  l3.addChild( l32 );
 
   Node r;
   r.data( 0 );
-  r.addChild( l1 );
-  r.addChild( l2 );
-  r.addChild( l3 );
+  r.addChild( l1 );  r.addChild( l2 );  r.addChild( l3 );
 
   t.root( r );
 }
